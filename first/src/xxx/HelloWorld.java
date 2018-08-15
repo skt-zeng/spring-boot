101e44573322b2f504a312133391031cfd91e83b
skt-zeng <skt_zeng@163.com>
2018-08-15 15:12:37 +0800

    增加一条输出

--- first/src/xxx/HelloWorld.java
+++ first/src/xxx/HelloWorld.java
@@ -3,6 +3,7 @@ package xxx;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello");
+        System.out.println("上传成功");
 
 
     }
---
+++ first/src/xxx/xixihaha.java
@@ -0,0 +1,8 @@
+package xxx;
+
+public class xixihaha {
+
+    public static void main(String[] args){
+        System.out.println("输出函数");
+    }
+}
